8ad8daf8791c182064f122b7ba1c974ea8100ee9
anyalauria <anyalauria@gmail.com>
2022-06-19 17:28:52 -0700

    Added resize ability and color modes

--- etchsketch/main.js
+++ etchsketch/main.js
@@ -1,6 +1,19 @@
 const container = document.querySelector('#container');
-let gridSize = document.querySelector('input');
+const blackMode = document.querySelector('#black');
+const rainbowMode = document.querySelector('#rainbow');
+const eraseMode = document.querySelector('#erase');
+const clearBtn = document.querySelector('#clear');
+const apply = document.querySelector('#apply');
 
+let gridSize = document.querySelector('#update-size');
+let dimensions = document.querySelector('#dimensions');
+
+let currSize = 16;
+let currMode = 'black';
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 function makeGrid(rows, cols) {
     container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
@@ -13,18 +26,38 @@ function makeGrid(rows, cols) {
 
         }
     }
+
+    let gridItems = document.getElementsByClassName("grid-item");
+
+    for(let i = 0; i < gridItems.length; i++){
+        gridItems[i].addEventListener("mouseover", function(event){
+            if (!mouseDown) return;
+            if (currMode==='black'){event.target.style.backgroundColor = "black";}
+            else if (currMode === 'rainbow') {
+                const R = Math.floor(Math.random() * 256)
+                const G = Math.floor(Math.random() * 256)
+                const B = Math.floor(Math.random() * 256)
+                event.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
+            }
+            else if (currMode === 'erase'){event.target.style.backgroundColor = '#fefefe'}
+
+        }, false);
+    }
   }
 
 makeGrid(16, 16);
 
-let gridItems = document.getElementsByClassName("grid-item");
+gridSize.addEventListener('change', function(event){
+    currSize = gridSize.value;
+    dimensions.textContent = `${currSize} x ${currSize}`
+})
 
-for(let i = 0; i < gridItems.length; i++){
-    gridItems[i].addEventListener("mouseover", function(event){
-        event.target.style.backgroundColor = "purple";
-
-    }, false);
-
-    
-}
-
+rainbowMode.addEventListener('click', () => {currMode = 'rainbow';})
+blackMode.addEventListener('click', () => {currMode = 'black';})
+eraseMode.addEventListener('click', () => {currMode = 'erase';})
+clearBtn.addEventListener('click', () => {
+    container.innerHTML = '';
+    makeGrid(currSize,currSize);})
+apply.addEventListener('click', () => {
+    container.innerHTML = '';
+    makeGrid(currSize,currSize);})
